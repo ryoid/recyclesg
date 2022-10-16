ee6548459efd3e7d52cde416afceed04f8999dc7
Ryan Conceicao <ryoid@users.noreply.github.com>
2022-10-16 23:19:22 +0800

    fixes

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,24 @@
 <template>
-  <div>
-    Some default layout shared across all pages
-    <slot />
-  </div>
+  <NConfigProvider inline-theme-disabled>
+    <NMessageProvider>
+      <NNotificationProvider>
+        <NLoadingBarProvider>
+          <NDialogProvider>
+            Some default layout shared across all pages
+            <slot />
+          </NDialogProvider>
+        </NLoadingBarProvider>
+      </NNotificationProvider>
+    </NMessageProvider>
+  </NConfigProvider>
 </template>
+
+<script lang="ts" setup>
+import {
+  NConfigProvider,
+  NDialogProvider,
+  NLoadingBarProvider,
+  NMessageProvider,
+  NNotificationProvider,
+} from 'naive-ui'
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,6 @@
     <h1 class="text-3xl font-bold underline">
       Hello world!
     </h1>
-    <DatePicker />
     <Welcome />
   </div>
 </template>
