2c80e0d8c674a77168fc9dab73a1642ea77d187c
Ryan Conceicao <ryoid@users.noreply.github.com>
2022-10-16 21:48:21 +0800

    add tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  css: [
+    '@/assets/css/tailwind.css',
+  ],
+  build: {
+    postcss: {
+      postcssOptions: require('./postcss.config.js'),
+    },
+  }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     <Welcome />
   </div>
 </template>
