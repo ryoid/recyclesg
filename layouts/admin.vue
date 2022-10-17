339ff91cc93734bdbbf62315651b3844c131a581
ryoid <ryanjconceicao@gmail.com>
2022-10-17 13:20:20 +0800

    updated layouts

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    Default Layout
+    Admin Layout
     <slot />
   </div>
 </template>
---
+++ pages/app.vue
@@ -0,0 +1,25 @@
+<template>
+  <NConfigProvider inline-theme-disabled>
+    <NMessageProvider>
+      <NNotificationProvider>
+        <NLoadingBarProvider>
+          <NDialogProvider>
+            <NuxtLayout>
+              <NuxtPage />
+            </NuxtLayout>
+          </NDialogProvider>
+        </NLoadingBarProvider>
+      </NNotificationProvider>
+    </NMessageProvider>
+  </NConfigProvider>
+</template>
+
+<script lang="ts" setup>
+import {
+  NConfigProvider,
+  NDialogProvider,
+  NLoadingBarProvider,
+  NMessageProvider,
+  NNotificationProvider,
+} from 'naive-ui'
+</script>
