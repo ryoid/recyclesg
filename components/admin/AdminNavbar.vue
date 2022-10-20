c9c4178eeb84bd21140b33f71d5f0186f3329e13
ryoid <ryanjconceicao@gmail.com>
2022-10-21 06:20:19 +0800

    fix: links

--- components/admin/AdminNavbar.vue
+++ components/admin/AdminNavbar.vue
@@ -2,21 +2,21 @@
   <nav
     class="hidden sm:flex sm:w-20 lg:w-[200px] h-screen fixed left-0 top-0 bottom-0 bg-white border-r border-r-neutral-100  flex-col justify-center">
     <ul>
-      <a href="/admin">
+      <NuxtLink href="/admin">
         <li class="py-4 px-3 hover:bg-neutral-100 cursor-pointer">
           Dashboard
         </li>
-      </a>
-      <a href="/admin/requests">
+      </NuxtLink>
+      <NuxtLink href="/admin/requests">
         <li class="py-4 px-3 hover:bg-neutral-100 cursor-pointer">
           Requests
         </li>
-      </a>
-      <a href="/admin/database">
+      </NuxtLink>
+      <NuxtLink href="/admin/database">
         <li class="py-4 px-3 hover:bg-neutral-100 cursor-pointer">
           Database
         </li>
-      </a>
+      </NuxtLink>
     </ul>
 
     <ul class="absolute bottom-0 pb-2 w-full">
--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,104 +1,106 @@
 <template>
-  <AdminHeader title="Dashboard" subtitle="Your overview" />
+  <div>
+    <AdminHeader title="Dashboard" subtitle="Your overview" />
 
-  <div class="grid grid-cols-1 gap-2 md:grid-cols-2">
-    <div class="md:col-span-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
-      <AdminDashboardCard title="Top 10 searches today" class="lg:col-span-1" accent-line="glow">
-        <AdminDashboardWordTable />
+    <div class="grid grid-cols-1 gap-2 md:grid-cols-2">
+      <div class="md:col-span-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
+        <AdminDashboardCard title="Top 10 searches today" class="lg:col-span-1" accent-line="glow">
+          <AdminDashboardWordTable />
+        </AdminDashboardCard>
+        <div class="col-span-1 lg:col-span-2 grid grid-rows-2 md:grid-rows-3 gap-2 auto-rows-auto auto-cols-auto">
+          <AdminDashboardCard title="Search Activity" class="flex md:hidden lg:flex row-span-2 md:col-span-1"
+            accent-line="glow">
+            <p v-if="!data.completed_requests && pending">Fetching data...</p>
+            <p v-else-if="error">An error occurred :(</p>
+            <AdminDashboardActivityChart v-else id="search-activity" :data="searchActivity" />
+          </AdminDashboardCard>
+          <AdminDashboardCard class="row-span-1 md:row-span-3 lg:row-span-1 flex md:flex-col lg:flex-row flex-wrap"
+            accent-line="glow">
+
+            <div class="flex md:flex-col lg:flex-row flex-wrap justify-around h-full w-full">
+              <div class="flex flex-col items-center justify-center">
+                <div class="font-semibold" style="font-size: 60px">
+                  {{ data.counts.users }}
+                </div>
+                <h2 class="font-semibold">Users</h2>
+                <p class="text-gray-600">In the past 24 hrs</p>
+              </div>
+              <div class="flex flex-col items-center justify-center">
+                <div class="font-semibold" style="font-size: 60px">
+                  {{ data.counts.bookings }}
+                </div>
+                <h2 class="font-semibold">Bookings</h2>
+                <p class="text-gray-600">Today</p>
+              </div>
+              <div class="flex flex-col items-center justify-center">
+                <div class="font-semibold" style="font-size: 60px">
+                  {{ data.counts.otherthing }}
+                </div>
+                <h2 class="font-semibold">Bookings</h2>
+                <p class="text-gray-600">Today</p>
+              </div>
+            </div>
+          </AdminDashboardCard>
+        </div>
+
+      </div>
+
+      <AdminDashboardCard title="Search Activity" class="hidden md:flex lg:hidden col-span-2" accent-line="glow">
+        <p v-if="!data.completed_requests && pending">Fetching data...</p>
+        <p v-else-if="error">An error occurred :(</p>
+        <AdminDashboardActivityChart id="search-activity-mobile" :data="searchActivity" />
       </AdminDashboardCard>
-      <div class="col-span-1 lg:col-span-2 grid grid-rows-2 md:grid-rows-3 gap-2 auto-rows-auto auto-cols-auto">
-        <AdminDashboardCard title="Search Activity" class="flex md:hidden lg:flex row-span-2 md:col-span-1"
-          accent-line="glow">
-          <p v-if="!data.completed_requests && pending">Fetching data...</p>
+
+      <AdminDashboardCard title="Pending Requests" :subtitle="`There are ${data.pending_requests_count} pending tasks`"
+        link="/admin/requests?status=pending" class="col-span-1"
+        :accent-line="data.pending_requests.length <= 0 ? undefined : 'line'">
+        <div class=" h-full">
+          <p v-if="!data.pending_requests && pending">Fetching mountains...</p>
           <p v-else-if="error">An error occurred :(</p>
-          <AdminDashboardActivityChart v-else id="search-activity" :data="searchActivity" />
-        </AdminDashboardCard>
-        <AdminDashboardCard class="row-span-1 md:row-span-3 lg:row-span-1 flex md:flex-col lg:flex-row flex-wrap"
-          accent-line="glow">
-
-          <div class="flex md:flex-col lg:flex-row flex-wrap justify-around h-full w-full">
-            <div class="flex flex-col items-center justify-center">
-              <div class="font-semibold" style="font-size: 60px">
-                {{ data.counts.users }}
-              </div>
-              <h2 class="font-semibold">Users</h2>
-              <p class="text-gray-600">In the past 24 hrs</p>
+          <div v-else-if="data.pending_requests.length <= 0"
+            class="flex flex-col items-center justify-center h-full md:px-16 py-8">
+            <div class="hidden lg:block relative aspect-[8/6] w-full overflow-hidden max-w-[300px]">
+              <img src="~/assets/img/illustration/completed_tasks.svg" class="absolute inset-0" />
             </div>
-            <div class="flex flex-col items-center justify-center">
-              <div class="font-semibold" style="font-size: 60px">
-                {{ data.counts.bookings }}
-              </div>
-              <h2 class="font-semibold">Bookings</h2>
-              <p class="text-gray-600">Today</p>
-            </div>
-            <div class="flex flex-col items-center justify-center">
-              <div class="font-semibold" style="font-size: 60px">
-                {{ data.counts.otherthing }}
-              </div>
-              <h2 class="font-semibold">Bookings</h2>
-              <p class="text-gray-600">Today</p>
-            </div>
+            <p class="text-gray-800 text-center lg:pt-4">You have no pending requests</p>
           </div>
-        </AdminDashboardCard>
-      </div>
-
-    </div>
-
-    <AdminDashboardCard title="Search Activity" class="hidden md:flex lg:hidden col-span-2" accent-line="glow">
-      <p v-if="!data.completed_requests && pending">Fetching data...</p>
-      <p v-else-if="error">An error occurred :(</p>
-      <AdminDashboardActivityChart id="search-activity-mobile" :data="searchActivity" />
-    </AdminDashboardCard>
-
-    <AdminDashboardCard title="Pending Requests" :subtitle="`There are ${data.pending_requests_count} pending tasks`"
-      link="/admin/requests?status=pending" class="col-span-1"
-      :accent-line="data.pending_requests.length <= 0 ? undefined : 'line'">
-      <div class=" h-full">
-        <p v-if="!data.pending_requests && pending">Fetching mountains...</p>
-        <p v-else-if="error">An error occurred :(</p>
-        <div v-else-if="data.pending_requests.length <= 0"
-          class="flex flex-col items-center justify-center h-full md:px-16 py-8">
-          <div class="hidden lg:block relative aspect-[8/6] w-full overflow-hidden max-w-[300px]">
-            <img src="~/assets/img/illustration/completed_tasks.svg" class="absolute inset-0" />
-          </div>
-          <p class="text-gray-800 text-center lg:pt-4">You have no pending requests</p>
+          <ul v-else class="-mx-3">
+            <li v-for="request of data.pending_requests">
+              <RecycleRequestTaskCard :request="request" />
+            </li>
+          </ul>
+          <NuxtLink v-if="data.pending_requests_count >= 4" href="/admin/requests"
+            class="flex justify-center text-blue-500 transition-opacity opacity-70 pt-2 hover:opacity-100">View all
+            requests
+          </NuxtLink>
         </div>
-        <ul v-else class="-mx-3">
-          <li v-for="request of data.pending_requests">
-            <RecycleRequestTaskCard :request="request" />
-          </li>
-        </ul>
-        <NuxtLink v-if="data.pending_requests_count >= 4" href="/admin/requests"
-          class="flex justify-center text-blue-500 transition-opacity opacity-70 pt-2 hover:opacity-100">View all
-          requests
-        </NuxtLink>
-      </div>
-    </AdminDashboardCard>
+      </AdminDashboardCard>
 
-    <AdminDashboardCard title="Completed Requests"
-      :subtitle="data.completed_requests.length <= 0 ? undefined : `Completed ${data.completed_requests_today_count} tasks today`"
-      link="/admin/requests" class="col-span-1">
-      <div>
-        <p v-if="!data.completed_requests && pending">Fetching mountains...</p>
-        <p v-else-if="error">An error occurred :(</p>
-        <div v-else-if="data.pending_requests.length <= 0"
-          class="flex flex-col items-center justify-center h-full lg:px-16 py-8">
-          <div class="hidden lg:block relative aspect-[1/1.05] w-full overflow-hidden max-w-[300px]">
-            <img src="~/assets/img/illustration/no_tasks.svg" class="absolute inset-0" />
+      <AdminDashboardCard title="Completed Requests"
+        :subtitle="data.completed_requests.length <= 0 ? undefined : `Completed ${data.completed_requests_today_count} tasks today`"
+        link="/admin/requests" class="col-span-1">
+        <div>
+          <p v-if="!data.completed_requests && pending">Fetching mountains...</p>
+          <p v-else-if="error">An error occurred :(</p>
+          <div v-else-if="data.pending_requests.length <= 0"
+            class="flex flex-col items-center justify-center h-full lg:px-16 py-8">
+            <div class="hidden lg:block relative aspect-[1/1.05] w-full overflow-hidden max-w-[300px]">
+              <img src="~/assets/img/illustration/no_tasks.svg" class="absolute inset-0" />
+            </div>
+            <p class="text-gray-800 text-center lg:pt-4">You have no recycle requests</p>
           </div>
-          <p class="text-gray-800 text-center lg:pt-4">You have no recycle requests</p>
-        </div>
-        <ul v-else class="[&_img]:saturate-50 -mx-3">
-          <li v-for="request of data.completed_requests">
-            <RecycleRequestTaskCard :request="request" />
-          </li>
-        </ul>
-        <!-- <NuxtLink href="/admin/requests"
+          <ul v-else class="[&_img]:saturate-50 -mx-3">
+            <li v-for="request of data.completed_requests">
+              <RecycleRequestTaskCard :request="request" />
+            </li>
+          </ul>
+          <!-- <NuxtLink href="/admin/requests"
           class="flex justify-center text-blue-500 transition-opacity opacity-70 pt-2 hover:opacity-100">View all
           requests
         </NuxtLink> -->
-      </div>
-    </AdminDashboardCard>
+        </div>
+      </AdminDashboardCard>
+    </div>
   </div>
 </template>
 
