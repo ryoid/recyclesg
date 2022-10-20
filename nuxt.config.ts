08a9dd3190044e5af18de09e0107b7ddbf7f4931
ryoid <ryanjconceicao@gmail.com>
2022-10-20 23:59:33 +0800

    feat: add primevue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,28 +1,32 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   css: [
-    '@/assets/css/tailwind.css',
+    "@/assets/css/tailwind.css",
+    "primevue/resources/themes/saga-blue/theme.css",
+    "primevue/resources/primevue.css",
+    "primeicons/primeicons.css",
   ],
   build: {
     postcss: {
-      postcssOptions: require('./postcss.config.js'),
+      postcssOptions: require("./postcss.config.js"),
     },
     transpile:
-      process.env.NODE_ENV === 'production'
+      process.env.NODE_ENV === "production"
         ? [
-            'naive-ui',
-            'vueuc',
-            '@css-render/vue3-ssr',
-            '@juggle/resize-observer'
+            "primevue",
+            "naive-ui",
+            "vueuc",
+            "@css-render/vue3-ssr",
+            "@juggle/resize-observer",
           ]
-        : ['@juggle/resize-observer']
+        : ["primevue", "@juggle/resize-observer"],
   },
   vite: {
     optimizeDeps: {
       include:
-        process.env.NODE_ENV === 'development'
-          ? ['naive-ui', 'vueuc', 'date-fns-tz/esm/formatInTimeZone']
-          : []
-    }
-  }
-})
+        process.env.NODE_ENV === "development"
+          ? ["naive-ui", "vueuc", "date-fns-tz/esm/formatInTimeZone"]
+          : [],
+    },
+  },
+});
