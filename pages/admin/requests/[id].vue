61b375bf245f0b06706b7f638ef152caca51a269
MuhammadAshraff <muhammadas.2021@scis.smu.edu.sg>
2022-11-01 23:40:15 +0800

    removed file upload button

--- pages/admin/requests/[id].vue
+++ pages/admin/requests/[id].vue
@@ -11,9 +11,6 @@
             <!-- <Image src="https://media.istockphoto.com/vectors/thumbnail-image-vector-graphic-vector-id1147544807?k=20&m=1147544807&s=612x612&w=0&h=pBhz1dkwsCMq37Udtp9sfxbjaMl27JUapoyYpQm0anc=" alt="Image Text" /> -->
             <img class="object-contain" src="https://media.istockphoto.com/vectors/thumbnail-image-vector-graphic-vector-id1147544807?k=20&m=1147544807&s=612x612&w=0&h=pBhz1dkwsCMq37Udtp9sfxbjaMl27JUapoyYpQm0anc=" alt="">
           </div>
-          <div>
-            <FileUpload mode="basic" name="demo[]" url="./upload" />
-          </div>
         </div>
      
         <div class="col-span-3">
