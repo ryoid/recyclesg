d43e796c9bf2024cd9e95650223d8d078ec92e77
ryoid <ryanjconceicao@gmail.com>
2022-11-02 17:28:11 +0800

    fix: table, remove unused

--- pages/admin/requests/[id].vue
+++ pages/admin/requests/[id].vue
@@ -11,7 +11,7 @@
       <div>
         <h1 class="text-3xl font-bold underline">
           Admin / Requests / [{{ id }}] detail
-        </h1> 
+        </h1>
       </div>
 
       <div class="container">
@@ -26,19 +26,19 @@
           </div>
         </div> -->
         <div class="grid grid-cols-2 gap-4 mt-5">
-            <div class="col-span-2 lg:col-span-1">
-              <h2 class="text-xl mb-2">Name</h2>
-              <InputText class="w-full" type="text" :value="data.title" />
-              <!-- <input type="text" class="border-solid border-gray-400 border-2 p-3 md:text-xl w-full" placeholder="Name" :value="data.name"/> -->
+          <div class="col-span-2 lg:col-span-1">
+            <h2 class="text-xl mb-2">Name</h2>
+            <InputText class="w-full" type="text" :value="data.title" />
+            <!-- <input type="text" class="border-solid border-gray-400 border-2 p-3 md:text-xl w-full" placeholder="Name" :value="data.name"/> -->
 
-            </div>
-            <div class="col-span-2 lg:col-span-1">
-              <h2 class="text-xl mb-2">Material</h2>
-              <InputText class="w-full" type="text" :value="data.status"/>
-              <!-- <input type="text" class="border-solid border-gray-400 border-2 p-3 md:text-xl w-full" placeholder="Name" :value="data.material"/> -->
-
-            </div>
           </div>
+          <div class="col-span-2 lg:col-span-1">
+            <h2 class="text-xl mb-2">Material</h2>
+            <InputText class="w-full" type="text" :value="data.status" />
+            <!-- <input type="text" class="border-solid border-gray-400 border-2 p-3 md:text-xl w-full" placeholder="Name" :value="data.material"/> -->
+
+          </div>
+        </div>
 
         <!-- tailwindcss radio button -->
         <div class="mt-6">
@@ -58,11 +58,15 @@
 
         <div class="mt-6">
           <label for="message" class="block mb-2 font-medium text-gray-900 text-xl">Description</label>
-          <textarea :value="data.description" id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-md border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="Your message..."></textarea>
+          <textarea :value="data.description" id="message" rows="4"
+            class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-md border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+            placeholder="Your message..."></textarea>
         </div>
 
         <div class="grid grid-cols-1 place-items-end mt-6">
-          <button @click="submitForm" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-md text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Add Entry</button>
+          <button @click="submitForm" type="button"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-md text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Add
+            Entry</button>
           <!-- <button v-else type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-md text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Add Entry</button> -->
         </div>
       </div>
@@ -80,15 +84,15 @@ const options = ref(""); // remove this later
 const route = useRoute()
 const id = route.params.id
 const { data, pending, refresh, error } = await useFetch(`/api/admin/recyclerequests/${id}`, {
-  
+
 })
 
-const submitForm  = async () => {
-  console.log("submit form", data)
-  await $fetch(`/api/admin/recyclerequests/${id}`, {
-    method: "PUT",
-    body: JSON.stringify(data),
-  })
+const submitForm = async () => {
+  console.log("submit form", data)
+  await $fetch(`/api/admin/recyclerequests/${id}`, {
+    method: "PUT",
+    body: JSON.stringify(data),
+  })
 }
 </script>
 
--- server/api/admin/recyclable/index.ts
+++ server/api/admin/recyclable/index.ts
@@ -2,11 +2,18 @@ import { Recyclable } from "~~/server/types";
 import jsonData from "./data.json";
 
 function getRecyclableData(): Recyclable[] {
-  return jsonData.map((d) => ({
-    ...d,
-    tags: ["test", "two"],
-    createdAt: new Date().toISOString(),
-  }));
+  const ids = new Map<any, boolean>();
+  return jsonData
+    .map((d) => ({
+      ...d,
+      tags: ["test", "two"],
+      createdAt: new Date().toISOString(),
+    }))
+    .filter((d) => {
+      if (ids.get(d.id)) return false;
+      ids.set(d.id, true);
+      return true;
+    });
 }
 
 export default defineEventHandler((event) => {
