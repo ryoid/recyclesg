fff76b5f5653e95b1d6671cdf8c3c115237607c3
zhiweit <theanzhiwei@gmail.com>
2022-11-03 01:56:16 +0800

    used divs instead of primevue accordion for recycling results

---
+++ components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav class="bg-slate-100" >
+    <div class="flex ">
+      <ul class="flex grid grid-cols-4">
+        <NavBarLink link="/home" name="home" label="Check" icon="pi pi-search" />
+        <NavBarLink link="/book/book1" name="book" label="Book" icon="pi pi-calendar" />
+        <NavBarLink link="/login" name="home" label="Login" icon="pi pi-user" class="ml-auto" />
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts" setup>
+
+</script>
---
+++ components/NavBarLink.vue
@@ -0,0 +1,24 @@
+<template>
+  <NuxtLink :href="props.link">
+    
+    <li 
+    :class="['py-4 px-4 cursor-pointer hover:bg-slate-200']" >
+    <i :class="[icon, 'pr-1']" style="font-size: 1rem"></i>
+      {{ props.label }}
+    </li>
+  </NuxtLink>
+</template>
+
+
+<script setup lang="ts">
+export type Props = {
+  link: string
+  label: string
+  name: string
+  icon: string
+}
+
+const props = defineProps<Props>()
+
+const route = useRoute()
+</script>
