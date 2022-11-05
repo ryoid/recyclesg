7160c16ee8fbf62758f985cb92c6393274d04960
kelvinlohwy <kelvinlohwenyong@gmail.com>
2022-11-05 16:30:06 +0800

    person object

--- pages/book/book2.vue
+++ pages/book/book2.vue
@@ -39,56 +39,82 @@ hr{
             Your details
         </h3>
     </div>
-
-    <div>
-        <p>Name</p>
-        <InputText type="text" v-model="data.nameInput" />
-        <p>Email</p>
-        <InputText type="text" v-model="data.emailInput" />
-        <p>Contact Number</p>
-        <InputNumber type="number" :useGrouping="false" v-model="data.phoneInput"/>
-    </div>
-
-    <br>
-        <hr>
-    <br>
-
-    <div>
-        <h3>Collection Location</h3>
-        <div class = "inline">
-            <p>Address</p>
-            <InputText type="text" v-model="data.addressInput" size="50" />
-        </div>
-        <div class = "inline">
-            <p>Postal Code</p>
-            <InputNumber type="number" :useGrouping="false" v-model="data.postalInput"/>
-        </div>
-    </div>
-
-    <br>
-    <hr>
-    <br>
-   
-    <div>
-        <h3>Collection Items</h3>
-        <FileUpload name="demo[]" url="./upload" :multiple="true" accept="image/*" />
-        <div>
-            <p>Description (optional)</p>
-            <Textarea v-model="data.descInput" rows="10" cols="80" />
-        </div>
-        <br>
-        <Button v-if="data.nameInput == '' || data.emailInput == '' || data.phoneInput == null || data.addressInput == '' || data.postalInput == null" @click="showAlert2()" label="Submit" style = "margin-left:4px">Book Collection</Button>
-        <Button v-else onclick="location.href = './book1'" label="Submit" style = "margin-left:4px">Book Collection</Button>          
-    </div>
-  </template>
-  
-  <script lang="ts" setup>
-   
-    const { data, pending, refresh, error } = await useFetch('/api/admin/book2', {
-    });
     
-    function showAlert2() {
-        alert("Please fill in all the details!");
+    <form>
+        <div>
+            <p>Name</p>
+            <InputText type="text" v-model="person.name" />
+            <p>Email</p>
+            <InputText type="text" v-model="person.email" />
+            <p>Contact Number</p>
+            <InputNumber type="number" :useGrouping="false" v-model="person.phone"/>
+        </div>
+
+        <br>
+            <hr>
+        <br>
+
+        <div>
+            <h3>Collection Location</h3>
+            <div class = "inline">
+                <p>Address</p>
+                <InputText type="text" v-model="person.address" size="50" />
+            </div>
+            <div class = "inline">
+                <p>Postal Code</p>
+                <InputNumber type="number" :useGrouping="false" v-model="person.postal"/>
+            </div>
+        </div>
+
+        <br>
+        <hr>
+        <br>
+    
+        <div>
+            <h3>Collection Items</h3>
+            <FileUpload name="demo[]" url="./upload" :multiple="true" accept="image/*"/>
+            <div>
+                <p>Description (optional)</p>
+                <Textarea v-model="person.desc" rows="10" cols="80" />
+            </div>
+            <br>
+            <Button @click= "showAlert2()" label="Submit"  style = "margin-left:4px">Book Collection</Button>          
+        </div>
+    </form>
+  </template>
+
+  <script>
+    export default{
+        data(){
+            return{
+                'person' : {
+                    'name' : '',
+                    'email': '',
+                    'phone': null,
+                    'address': '',
+                    'postal': null,
+                    'image': '',
+                    'desc': '',
+                }
+            }
+        },
+        methods:
+        {
+            showAlert2(){
+                if(this.person.name == '' || this.person.email == '' || this.person.phone == null || this.person.address == '' || this.person.postal == null){
+
+                return alert("Please fill in all the fields")
+                }
+                else{
+                    console.log(this.person)
+                    return alert("Your booking has been submitted");
+                    
+                }
+            }
+        }
     }
+    
+    
+
     
   </script>
