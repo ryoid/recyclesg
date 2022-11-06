c3f4730d6020f75367531f3a2b57fb01a313b836
kelvinlohwy <kelvinlohwenyong@gmail.com>
2022-11-06 21:52:41 +0800

    1

--- pages/book/book1.vue
+++ pages/book/book1.vue
@@ -12,54 +12,78 @@ div {
     display: inline;
 }
 
-.inline-block{
+.inline-block {
     display: inline-block;
-    vertical-align:top;
+    vertical-align: top;
 }
 
-.margin-right{
+.margin-right {
     margin-right: 100px;
 }
-
 </style>
 
 
 
 <template>
-    <nav_bar/>
-    <h2>Book Collection</h2>
     <div>
-        <p>
-            Lorem,ipsum dolor sit amet consectetur adipisicing elit. Itaque harum corrupti temporibus dignissimos, deleniti maiores aut sequi voluptatibus nihil eveniet quasi id esse voluptatem, ipsum unde libero quae doloremque inventore!
-        </p>
-    </div>
-    <br>
+        <nav_bar />
+        <h2>Book Collection</h2>
+        <div>
+            <p>
+                Lorem,ipsum dolor sit amet consectetur adipisicing elit. Itaque harum corrupti temporibus dignissimos,
+                deleniti maiores aut sequi voluptatibus nihil eveniet quasi id esse voluptatem, ipsum unde libero quae
+                doloremque inventore!
+            </p>
+        </div>
+        <br>
         <div class="flex justify-center ">
-            <Calendar :disabled-dates="data.disabledDateArray" v-model="data.selectedDate" :inline="true" :minDate="new Date(data.minDateValue)" :maxDate="maxDateValue" :disabledDates="data.disabledDateArray" class = "inline-block margin-right" />
-            <div class = "inline-block">
-                <Dropdown v-model="data.selectedTiming" :options="data.timings"  placeholder="Select a timing" />
-                <div class = "inline-block ">
-                    <Button v-if="data.selectedDate == '' || data.selectedTiming == ''" @click="showAlert()" label="Continue"/>
-                    <Button v-else onclick="location.href = './book2'" label="Continue"/>
+            <Calendar :disabled-dates="data.unavailableDays.map(day => new Date(day))" v-model="form.selectedDate"
+                :inline="true" :minDate="form.minDateValue" :maxDate="form.maxDateValue"
+                class="inline-block margin-right" />
+            <div class="inline-block">
+                <!-- <Dropdown v-model="form.selectedTiming" :options="form.timings" placeholder="Select a timing" /> -->
+                Timings
+                <ul>
+                    <li
+                        v-for="slot in data.availableSlots.find(d => isSameDay(new Date(d[0].date), form.selectedDate))">
+                        {{ slot.date }}
+                    </li>
+                </ul>
+                <div class="inline-block ">
+                    <Button v-if="form.selectedDate == '' || form.selectedTiming == ''" @click="showAlert()"
+                        label="Continue" />
+                    <Button v-else onclick="location.href = './book2'" label="Continue" />
                 </div>
             </div>
-                <p>{{data.selectedDate}}</p>
-                <p>{{data.selectedTiming}}</p>
+            <p>{{ form.selectedDate }}</p>
+            <p>{{ form.selectedTiming }}</p>
+
+        </div>
+            <div>
+                {{ JSON.stringify(data.availableSlots, null, 2) }}
             </div>
-        <div>
-      
     </div>
 
-  </template>
+</template>
   
-  
-  <script lang="ts" setup>
-  
-    const { data, pending, refresh, error } = await useFetch('/api/admin/book1', {
-    });
+<script lang="ts" setup>
+import { addDays, isSameDay } from 'date-fns'
 
-    function showAlert(){
-            return alert("Please select a date and time");
-        }; 
+const form = ref({
+    timings: ["09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00"],
+    minDateValue: new Date(),
+    maxDateValue: addDays(new Date(), 14),
+    selectedDate: null,
+    selectedTiming: null,
 
-  </script>
+})
+
+const { data, pending, refresh, error } = await useFetch('/api/admin/bookings/available', {
+});
+
+
+function showAlert() {
+    return alert("Please select a date and time");
+};
+
+</script>
--- pages/book/book2.vue
+++ pages/book/book2.vue
@@ -9,8 +9,6 @@ h3 {
     font-family: "Times New Roman";
 }
 
-
-
 div {
     margin: 5px;
 }
@@ -21,108 +19,94 @@ div {
 
 hr {
     border-top: 2px solid rgb(138, 138, 138);
-
 }
 </style>
 
 <template>
-    <nav_bar/>
-    
-    <h2>Booking Details</h2>
-    <div id="date_time">
-        <p>
-            day, ## month - time
-        </p>
-            <br>
-        <h3>
-            Your details
-        </h3>
-    </div>
-    
-    <form>
-        <div>
-            <p>Name</p>
-            <InputText type="text" v-model="person.name" />
-            <p>Email</p>
-            <InputText type="text" v-model="person.email" />
-            <p>Contact Number</p>
-            <InputNumber type="number" :useGrouping="false" v-model="person.phone"/>
+    <div>
+        <nav_bar />
+
+        <h2>Booking Details</h2>
+        <div id="date_time">
+            <p>day, ## month - time</p>
+            <br />
+            <h3>Your details</h3>
         </div>
 
-        <br>
-            <hr>
-        <br>
-
-        <div>
-            <h3>Collection Location</h3>
-            <div class = "inline">
-                <p>Address</p>
-                <InputText type="text" v-model="person.address" size="50" />
-            </div>
-            <div class = "inline">
-                <p>Postal Code</p>
-                <InputNumber type="number" :useGrouping="false" v-model="person.postal"/>
-            </div>
-        </div>
-
-        <br>
-        <hr>
-        <br>
-    
-        <div>
-            <h3>Collection Items</h3>
-            <FileUpload name="demo[]" url="./upload" :multiple="true" accept="image/*"/>
+        <form>
             <div>
-                <p>Description (optional)</p>
-                <Textarea v-model="person.desc" rows="10" cols="80" />
+                <p>Name</p>
+                <InputText type="text" v-model="person.name" />
+                <p>Email</p>
+                <InputText type="text" v-model="person.email" />
+                <p>Contact Number</p>
+                <InputNumber type="number" :useGrouping="false" v-model="person.phone" />
             </div>
-            <br>
-            <Button @click= "showAlert2()" label="Submit"  style = "margin-left:4px">Book Collection</Button>          
-        </div>
-    </form>
-  </template>
 
-  <script>
-    export default{
-        data(){
-            return{
-                'person' : {
-                    'name' : '',
-                    'email': '',
-                    'phone': null,
-                    'address': '',
-                    'postal': null,
-                    'image': '',
-                    'desc': '',
-                }
-            }
-        },
-        methods:
-        {
-            showAlert2(){
-                if(this.person.name == '' || this.person.email == '' || this.person.phone == null || this.person.address == '' || this.person.postal == null){
+            <br />
+            <hr />
+            <br />
 
-                return alert("Please fill in all the fields")
-                }
-                else if (this.person.email.includes('@') == false){
-                    return alert("Please enter a valid email address")
-                }
-                else if(this.person.phone.toString().length != 8){
-                    return alert("Please enter a valid phone number")
-                }
-                else if(this.person.postal.toString().length != 6){
-                    return alert("Please enter a valid postal code")
-                }
-                else{
-                    console.log(this.person)
-                    return alert("Your booking has been submitted")
-                    
-                }
-            }
-        }
+            <div>
+                <h3>Collection Location</h3>
+                <div class="inline">
+                    <p>Address</p>
+                    <InputText type="text" v-model="person.address" size="50" />
+                </div>
+                <div class="inline">
+                    <p>Postal Code</p>
+                    <InputNumber type="number" :useGrouping="false" v-model="person.postal" />
+                </div>
+            </div>
+
+            <br />
+            <hr />
+            <br />
+
+            <div>
+                <h3>Collection Items</h3>
+                <FileUpload name="demo[]" url="./upload" :multiple="true" accept="image/*" />
+                <div>
+                    <p>Description (optional)</p>
+                    <Textarea v-model="person.desc" rows="10" cols="80" />
+                </div>
+                <br />
+                <Button @click="showAlert2()" label="Submit" style="margin-left: 4px">Book Collection</Button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script lang="ts" setup>
+const person = ref({
+    name: "",
+    email: "",
+    phone: null,
+    address: "",
+    postal: null,
+    image: "",
+    desc: "",
+});
+
+function showAlert2() {
+    console.log("show alert", person.value);
+    if (
+        person.value.name == "" ||
+        person.value.email == "" ||
+        person.value.phone == null ||
+        person.value.address == "" ||
+        person.value.postal == null
+    ) {
+        return alert("Please fill in all the fields");
+    } else if (person.value.email.includes("@") == false) {
+        return alert("Please enter a valid email address");
+    } else if (person.value.phone.toString().length != 8) {
+        return alert("Please enter a valid phone number");
+    } else if (person.value.postal.toString().length != 6) {
+        return alert("Please enter a valid postal code");
+    } else {
+        console.log(person.value);
+        return alert("Your booking has been submitted");
     }
-    
-    
-
-    
-  </script>
+}
+</script>
