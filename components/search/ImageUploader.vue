eb639ece3302503d918ff369528babdfe0900059
fafaqweqwe <101376007+fafaqweqwe@users.noreply.github.com>
2022-11-07 14:00:38 +0800

    Update ImageUploader.vue

--- components/search/ImageUploader.vue
+++ components/search/ImageUploader.vue
@@ -2,67 +2,85 @@
 
   <div>
 
-    <div class="container parent flex w-5/6 mx-auto">
-
-      <FileUpload
-        id = "fileupload" 
-        name="image[]" 
-        url="url" 
-        @upload="onAdvancedUpload($event)"
-        v-bind:fileLimit= 1
-        accept="image/*"
-        :max-file-size="1000000">
-        <template #header="{ chooseCallback, uploadCallback, clearCallback, files }">
-            <div class="flex flex-wrap justify-content-between align-items-center flex-1 gap-2">
-                <div class="flex gap-2">
-                    <Button @click="chooseCallback()" icon="pi pi-images" label="Choose a picture"></Button>
-                    <Button @click="openCamera()" icon="pi pi-times" label="Take a picture"></Button>
-                    <Button @click="uploadCallback()" icon="pi pi-cloud-upload" label="Upload" :disabled="!files || files.length === 0"></Button>
-                    <Button @click="clearCallback()" icon="pi pi-times" label="Cancel" :disabled="!files || files.length === 0"></Button>
-                    
-                </div>
-            </div>
-        </template>
-        <template #content>
-            <ul v-if="uploadedFiles && uploadedFiles[0]">
-                <li v-for="file of uploadedFiles[0]" :key="file">{{ file.name }} - {{ file.size }} bytes</li>
-            </ul>
-            <ul v-if="captures!=[]">
-                <li v-for="c of captures">
-                  <img v-bind:src="c" height="50" />
-                </li>              
-            </ul>
-        </template>
-        <template #empty>
-            <i id="uploadicon" class="col-12 pi pi-upload"></i>
-            <p class="col-12 text-center">Drag and drop files to here to upload.</p>
-            <p id="warntext">*Maximum file limit: 1</p>
-        </template>
-      </FileUpload>
-
-
+    <div class="container w-5/6 mx-auto">
+      <div class="row" v-if="!cameraOn">
+        <Button id="takePicture" @click="openCamera()" icon="pi pi-camera" label="Take a picture"></Button>
       </div>
-      <div v-if="cameraOn">
-        <div><video ref="video" id="video" width="640" height="480" autoplay>{{cameraPreview}}</video></div>
-        <div><button id="snap" v-on:click="capture()">Snap Photo</button></div>
-        <canvas ref="canvas" id="canvas" width="640" height="480"></canvas>
+      
+      <div class="row">
+        <FileUpload
+          v-if="!cameraOn"
+          id = "fileupload" 
+          name="image[]" 
+          url="url" 
+          @upload="onAdvancedUpload($event)"
+          v-bind:fileLimit= 1
+          accept="image/*"
+          :max-file-size="1000000">
+          <template #content>
+              <ul v-if="uploadedFiles && uploadedFiles[0]">
+                  <li v-for="file of uploadedFiles[0]" :key="file">{{ file.name }} - {{ file.size }} bytes</li>
+              </ul>
+          </template>
+          <template #empty>
+              <i id="uploadicon" class="col-12 pi pi-upload"></i>
+              <p class="col-12 text-center">Drag and drop files to here to upload.</p>
+              <p id="warntext">*Maximum file limit: 1</p>
+          </template>
+        </FileUpload>        
       </div>
-    </div>    
-  </template>
+
+      <div class="row">
+        <div id="takePicture"><video class="mx-auto" :hidden="!cameraOn" ref="video" id="video" width="640" height="480" autoplay>{{cameraPreview}}</video></div>
+        <div v-if="cameraOn">
+          <div class="flex">
+            <Button 
+              class="flex p-button-rounded w-1/3 mx-auto" 
+              id="snap" 
+              v-on:click="capture()" 
+              icon="pi pi-camera"></Button>
+          </div>
+          <canvas hidden ref="canvas" id="canvas" width="640" height="480"></canvas>
+        </div>         
+      </div>
+
+      <div class="row">
+        <Dialog v-model:visible="display" :closable="false">
+          <template #header>
+          <h3 :style="'font-size:20px;'" class="mx-auto">Upload image?</h3>
+        </template>
+
+        <ul>
+            <li v-for="c of captures">
+              <img v-bind:src="c" :width="width"/>
+            </li>              
+        </ul> 
+
+        <template #footer>
+          <Button label="No" icon="pi pi-times" class="p-button-text" v-on:click="clear()"/>
+              <Button label="Yes" icon="pi pi-check" autofocus />
+        </template>
+      </Dialog>
+      </div>
+    </div>
+   
+  </div>
+</template>
 
   <script>
       export default({
 
       data(){
         return {
-          uploadedFiles: null,
+          uploadedFiles: [],
           length: 0,
           previewImage: null,
           upload: null,
           video: {},
           canvas: {},
           captures: [],
-          cameraOn: false
+          cameraOn: false,
+          display: false,
         }
       },
 
@@ -79,17 +97,30 @@
             .getContext("2d")
             .drawImage(this.video, 0, 0, 640, 480);
           this.captures.push(canvas.toDataURL("image/png"));
+          this.display = true
         },
 
         cameraPreview(){
           if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
             navigator.mediaDevices.getUserMedia({ video: true, audio: false }).then(stream => {
               console.log(stream)
+              console.log(this.uploadedFiles)
               video.srcObject = stream;
+              this.width = this.video.width
               this.video.play();              
         })}
+        },
+
+        clear(){
+          this.canvas = {}
+          this.captures = []
+          this.display = false
         }
-      }, 
+      },
+      
+      mounted() {
+        this.cameraPreview()
+      }
       
   })
 
@@ -113,6 +144,16 @@
     text-align: center;
     color: red;
     font-size: small;
+  }
+
+  #takePicture{
+    margin-bottom: 10px;
+  }
+
+  #snap{
+    margin: auto;
+    min-width: 50px;
+    min-height: 50px;
   }
 
 </style>
