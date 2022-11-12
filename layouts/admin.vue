3690795e155f221a10bddc4e88b898acbe1b7c19
ryoid <ryanjconceicao@gmail.com>
2022-11-12 21:18:06 +0800

    admin layout padding bottom

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col bg-[#FAFAFB] min-h-screen">
+  <div class="flex flex-col bg-[#FAFAFB] min-h-screen pb-20">
     <AdminNavbar />
     <main class="container mx-auto px-2 sm:px-0">
       <slot />
