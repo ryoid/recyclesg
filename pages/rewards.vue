6e6200e4aa174ee6c4168d4f3e7de6943ea70e03
MuhammadAshraff <muhammadas.2021@scis.smu.edu.sg>
2022-11-13 19:14:43 +0800

    Create rewards.vue

---
+++ pages/rewards.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <div class="container">
+            <div class="grid-cols-1">
+            <!-- <h1 class="text-2xl">Rewards</h1> -->
+            <h1 class="flex items-center text-4xl font-extrabold dark:text-white">Rewards</h1>
+            </div>
+            <div class="flex flex-col items-center pb-10">
+                <img class="mb-3 w-24 h-24 rounded-full shadow-lg" src="https://www.treasury.gov.ph/wp-content/uploads/2022/01/male-placeholder-image.jpeg" />
+                <h5 class="mb-1 text-xl font-medium text-gray-900">Ryan</h5>
+                <span class="text-2xl font-extrabold text-black-800">Points: <span class="text-green-700">1500</span></span>
+                
+            </div>
+            <div class="grid-cols-1 mt-5">
+                <h2 class="text-2xl">My Rewards</h2>
+            </div>
+            <div class="grid grid-cols-3 gap-4 mt-5">
+              <div class="col-span-2 lg:col-span-1">
+                <a class="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md">
+                    <h5 class="mb-5 text-2xl font-bold tracking-tight text-gray-900 text-center">$10 Voucher</h5>
+                    <p class="font-normal text-xl text-green-700 text-center">500p</p>
+                </a>
+              </div>
+            </div>
+
+            <div class="grid-cols-1 mt-20">
+                <h2 class="text-2xl">List of Rewards</h2>
+            </div>
+            <div class="grid grid-cols-3 gap-4 mt-5">
+              <div class="col-span-2 lg:col-span-1">
+                <a class="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md">
+                    <h5 class="mb-5 text-2xl font-bold tracking-tight text-gray-900 text-center">$10 Voucher</h5>
+                    <p class="font-normal text-xl text-green-700 text-center">100p</p>
+                    <button type="submit" class="mt-5 w-full text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-md text-sm px-5 py-2.5 mr-2 mb-2">Redeem now!</button>
+                </a>
+              </div>
+              <div class="col-span-2 lg:col-span-1">
+                <a class="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md">
+                    <h5 class="mb-5 text-2xl font-bold tracking-tight text-gray-900 text-center">Reusable Bag</h5>
+                    <p class="font-normal text-xl text-green-700 text-center">200p</p>
+                    <button type="submit" class="mt-5 w-full text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-md text-sm px-5 py-2.5 mr-2 mb-2">Redeem now!</button>
+                </a>
+              </div>
+              <div class="col-span-2 lg:col-span-1">
+                <a class="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md">
+                    <h5 class="mb-5 text-2xl font-bold tracking-tight text-gray-900 text-center">Hydro Flask</h5>
+                    <p class="font-normal text-xl text-green-700 text-center">500p</p>
+                    <button type="submit" class="mt-5 w-full text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-md text-sm px-5 py-2.5 mr-2 mb-2">Redeem now!</button>
+                </a>
+              </div>
+            </div>
+           
+        </div>
+        
+    </div>
+</template>
