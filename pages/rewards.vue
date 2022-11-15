10ffed8f23606a3561e37019bbee47fb71793dac
MuhammadAshraff <muhammadas.2021@scis.smu.edu.sg>
2022-11-15 19:02:36 +0800

    Update rewards.vue

--- pages/rewards.vue
+++ pages/rewards.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
-    <div class="container">
-      <div class="grid-cols-1">
-        <!-- <h1 class="text-2xl">Rewards</h1> -->
-        <h1 class="flex items-center text-4xl font-extrabold dark:text-white">Rewards</h1>
-
+    <NuxtLink v-if="user.user?.email == null" class="flex items-center justify-center h-screen" to="/login">
+      <div class="text-white bg-teal-700 hover:bg-teal-800 focus:ring-4 focus:outline-none focus:ring-teal-300 font-medium rounded-lg text-sm p-10 text-center mr-3 md:mr-0">
+          <p class="text-2xl font-semibold">Log in to see the list of rewards you can claim!</p>
       </div>
+    </NuxtLink>
+
+ 
+    <div v-else class="container">
+      <div class="grid-cols-1">
+        <h1 class="flex items-center text-4xl font-extrabold dark:text-white">Rewards</h1>
+      </div>
+
+     
       <div class="flex flex-col items-center pb-10">
         <img class="mb-3 w-24 h-24 rounded-full shadow-lg"
           src="https://www.treasury.gov.ph/wp-content/uploads/2022/01/male-placeholder-image.jpeg" />
@@ -19,8 +26,8 @@
         <h2 class="text-2xl font-semibold">My Rewards</h2>
       </div>
       <div class="grid grid-cols-3 gap-4 mt-5">
-        <div v-for="reward in (profile.user?.rewards ?? [])" class="col-span-2 lg:col-span-1">
-          <a class="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md mb-3">
+        <div v-for="reward in (profile.user?.rewards ?? [])" class="col-span-3 lg:col-span-1">
+          <a class="block p-6 bg-white rounded-lg border border-gray-200 shadow-md mb-3 w-full ">
             <h5 class="mb-5 text-2xl font-bold tracking-tight text-gray-900 text-center">{{ reward.name }}</h5>
             <p class="font-normal text-xl text-green-700 text-center">{{ reward.points }}</p>
           </a>
@@ -31,8 +38,8 @@
         <h2 class="text-2xl font-semibold">List of Rewards</h2>
       </div>
       <div class="grid grid-cols-3 gap-4 mt-5">
-        <div v-for="reward in rewards.data.value" class="col-span-2 lg:col-span-1">
-          <a class="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md">
+        <div v-for="reward in rewards.data.value" class="col-span-3 lg:col-span-1">
+          <a class="block p-6 bg-white rounded-lg border border-gray-200 shadow-md w-full">
             <h5 class="mb-5 text-2xl font-bold tracking-tight text-gray-900 text-center">{{ reward.name }}</h5>
             <p class="font-normal text-xl text-green-700 text-center">{{ reward.points }}</p>
             <button type="submit" @click="redeemRewards(reward.id)"
@@ -40,8 +47,13 @@
               now!</button>
           </a>
         </div>
-
       </div>
+
+      <!-- <div>
+        <div class="bg-yellow-500/50 text-yellow-800 text-center p-4 rounded-lg justify-between items-center mt-6 mb-2">
+          You just earned 150 points
+        </div>
+      </div> -->
 
     </div>
 
@@ -63,6 +75,7 @@ let profile = ref({
   error: null,
 })
 
+// console.log(profile.value.user)
 // {{user.user?.uid}}
 
 watch(user, async (newUser) => {
@@ -88,11 +101,20 @@ const redeemRewards = async (id) => {
   await rewards.refresh()
   profile.value.user = await $fetch(`/api/user/${user.user?.uid}`).catch(err => {
     profile.value.error = err
+   
   })
-  //   console.log("reject form", data.value.status)    
 }
 
+// function checkLogin(){
+//   // redirect to login page if user is not logged in
+//   // if(!user.value.user){
+//   //   router.push('/login')
+//   // }
+//   console.log('test')
+//   console.log(user.user?.email)
+//   // return true
+// }
 
-
+// checkLogin()
 
 </script>
