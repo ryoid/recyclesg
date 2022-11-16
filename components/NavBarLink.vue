7e1f24c01256987609117066b07ebc797c98c616
ryoid <ryanjconceicao@gmail.com>
2022-11-16 09:13:02 +0800

    fix

--- components/NavBarLink.vue
+++ components/NavBarLink.vue
@@ -2,7 +2,7 @@
   <NuxtLink :href="props.link">
     <li 
     :class="['flex py-2 md:py-4 pr-4 pl-3 rounded', {
-      'bg-teal-700 md:bg-transparent text-white': route.name.toString().startsWith(props.name),
+      'bg-teal-700 md:bg-transparent text-white md:text-teal-700': route.name.toString().startsWith(props.name),
       'text-gray-700 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-teal-700': !route.name.toString().startsWith(props.name)
     }]" >
     <i :class="['', 'pr-1']" style="font-size: 1rem"></i>
